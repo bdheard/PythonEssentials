3ed81d8a1dbf7074177f41f0ce811036d040766e
Bruno, Daniel <dabruno@bethel.jw.org>
2021-04-16 19:09:57 -0400

    fix publish bugs

--- publish_slides.ps1
+++ publish_slides.ps1
@@ -14,21 +14,19 @@ pandoc -s Module10.md -o ..\output\Module10.pdf
 pandoc -s Module11.md -o ..\output\Module11.pdf
 pandoc -s Module12.md -o ..\output\Module12.pdf
 
-Set-Location -Path ..
-
-pandoc -t revealjs -s Slides\Module00.md -o output\Module00.html
-pandoc -t revealjs -s Slides\Module01.md -o output\Module01.html
-pandoc -t revealjs -s Slides\Module02.md -o output\Module02.html
-pandoc -t revealjs -s Slides\Module03.md -o output\Module03.html
-pandoc -t revealjs -s Slides\Module04.md -o output\Module04.html
-pandoc -t revealjs -s Slides\Module05.md -o output\Module05.html
-pandoc -t revealjs -s Slides\Module06.md -o output\Module06.html
-pandoc -t revealjs -s Slides\Module07.md -o output\Module07.html
-pandoc -t revealjs -s Slides\Module08.md -o output\Module08.html
-pandoc -t revealjs -s Slides\Module09.md -o output\Module09.html
-pandoc -t revealjs -s Slides\Module10.md -o output\Module10.html
-pandoc -t revealjs -s Slides\Module11.md -o output\Module11.html
-pandoc -t revealjs -s Slides\Module12.md -o output\Module12.html
+pandoc -t revealjs -s Module00.md -o ..\output\Module00.html
+pandoc -t revealjs -s Module01.md -o ..\output\Module01.html
+pandoc -t revealjs -s Module02.md -o ..\output\Module02.html
+pandoc -t revealjs -s Module03.md -o ..\output\Module03.html
+pandoc -t revealjs -s Module04.md -o ..\output\Module04.html
+pandoc -t revealjs -s Module05.md -o ..\output\Module05.html
+pandoc -t revealjs -s Module06.md -o ..\output\Module06.html
+pandoc -t revealjs -s Module07.md -o ..\output\Module07.html
+pandoc -t revealjs -s Module08.md -o ..\output\Module08.html
+pandoc -t revealjs -s Module09.md -o ..\output\Module09.html
+pandoc -t revealjs -s Module10.md -o ..\output\Module10.html
+pandoc -t revealjs -s Module11.md -o ..\output\Module11.html
+pandoc -t revealjs -s Module12.md -o ..\output\Module12.html
 
 # Rename-Item -Path "slides\module1.md" -NewName "Module01.md" -Force
 # Rename-Item -Path "slides\module2.md" -NewName "Module02.md" -Force
